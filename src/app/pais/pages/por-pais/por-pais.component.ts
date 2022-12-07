8036623b4b35c9cd38a7997c6f79b2f492cf3b10
jonathanbarzola <jonabl2102@gmail.com>
2022-12-07 14:42:08 -0500

    Paises App

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,6 +18,8 @@ export class PorPaisComponent {
   termino:  string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor( private paisService: PaisService ) {}
 
@@ -34,6 +41,19 @@ export class PorPaisComponent {
 
   sugerencias( termino: string ) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    
+    this.paisService.buscarPais(termino)
+      .subscribe( 
+        paises => this.paisesSugeridos = paises.splice(0,5),
+        (err) => this.paises = []
+      )
+
+  }
+
+  buscarSugerido( termino: string ) {
+    this.buscar( termino );
     
   }
 }
